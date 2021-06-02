6e97406d471ba7cfadb8690fe3c8ffadea2a4935
Igrium <thesam54123@gmail.com>
2021-06-01 17:21:40 -0700

    updated implementation of NBTBrowserController

--- src/main/java/org/scaffoldeditor/editormc/nbt_browser/NBTBrowserController.java
+++ src/main/java/org/scaffoldeditor/editormc/nbt_browser/NBTBrowserController.java
@@ -2,10 +2,12 @@ package org.scaffoldeditor.editormc.nbt_browser;
 
 import java.io.IOException;
 
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
@@ -13,21 +15,11 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import net.querz.nbt.io.NamedTag;
-import net.querz.nbt.tag.ByteArrayTag;
-import net.querz.nbt.tag.ByteTag;
 import net.querz.nbt.tag.CompoundTag;
-import net.querz.nbt.tag.DoubleTag;
-import net.querz.nbt.tag.FloatTag;
-import net.querz.nbt.tag.IntArrayTag;
-import net.querz.nbt.tag.IntTag;
 import net.querz.nbt.tag.ListTag;
-import net.querz.nbt.tag.LongArrayTag;
-import net.querz.nbt.tag.LongTag;
-import net.querz.nbt.tag.ShortTag;
-import net.querz.nbt.tag.StringTag;
 import net.querz.nbt.tag.Tag;
 
-public class NBTBrowserController {
+public final class NBTBrowserController {
 	
 	public final Image BYTE_ICON = new Image(getClass().getResourceAsStream("/assets/scaffold/ui/images/nbt/tag_byte.png"));
 	public final Image BYTE_ARRAY_ICON = new Image(getClass().getResourceAsStream("/assets/scaffold/ui/images/nbt/tag_byte_array.png"));
@@ -43,10 +35,39 @@ public class NBTBrowserController {
 	public final Image STRING_ICON = new Image(getClass().getResourceAsStream("/assets/scaffold/ui/images/nbt/tag_string.png"));
 	
 	@FXML
-	private TreeView<String> nbtTree;
+	private TreeView<NamedTag> nbtTree;
 	
 	private NamedTag tag;
 	private Parent root;
+	
+	@FXML
+	private void initialize() {
+		nbtTree.setCellFactory(tree -> {
+			TreeCell<NamedTag> cell = new TreeCell<NamedTag>() {
+				public void updateItem(NamedTag item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+						setGraphic(null);
+					} else {
+						Tag<?> tag = item.getTag();
+//						System.out.println(tag);
+						String textValue = tag.valueToString();
+						if (tag instanceof ListTag) {
+							textValue = ((ListTag<?>) tag).size()+" entries";
+						} else if (tag instanceof CompoundTag) {
+							textValue = ((CompoundTag) tag).size()+" entries";
+						}
+						setText(item.getName().length() > 0 ? item.getName()+": "+textValue : textValue);
+						setGraphic(new ImageView(getTagImage(tag.getID())));
+					}
+				}
+			};
+			
+			
+			return cell;
+		});
+	}
 	
 	/**
 	 * Load an NBT tag into the UI.
@@ -54,7 +75,7 @@ public class NBTBrowserController {
 	 */
 	public void loadNBT(NamedTag tag) {
 		this.tag = tag;
-		TreeItem<String> root = loadTag(tag);
+		TreeItem<NamedTag> root = loadTag(tag);
 		nbtTree.setRoot(root);
 	}
 	
@@ -65,94 +86,55 @@ public class NBTBrowserController {
 		return tag;
 	}
 	
-	protected TreeItem<String> loadTag(NamedTag in) {
-		String name = in.getName();
-		Tag<?> tag = in.getTag();
-		
-		if (tag.getID() == 1) {
-			return loadByteTag((ByteTag) tag, name);
-		} else if (tag.getID() == 2) {
-			return loadShortTag((ShortTag) tag, name);
-		} else if (tag.getID() == 3) {
-			return loadIntTag((IntTag) tag, name);
-		} else if (tag.getID() == 4) {
-			return loadLongTag((LongTag) tag, name);
-		} else if (tag.getID() == 5) {
-			return loadFloatTag((FloatTag) tag, name);
-		} else if (tag.getID() == 6) {
-			return loadDoubleTag((DoubleTag) tag, name);
-		} else if (tag.getID() == 7) {
-			return loadByteArrayTag((ByteArrayTag) tag, name);
-		} else if (tag.getID() == 8) {
-			return loadStringTag((StringTag) tag, name);
-		} else if (tag.getID() == 9) {
-			return loadListTag((ListTag<?>) tag, name);
-		} else if (tag.getID() == 10) {
-			return loadCompoundTag((CompoundTag) tag, name);
-		} else if (tag.getID() == 11) {
-			return loadIntArrayTag((IntArrayTag) tag, name);
-		} else if (tag.getID() == 12) {
-			return loadLongArrayTag((LongArrayTag) tag, name);
+	public TreeView<NamedTag> getNBTTree() {
+		return nbtTree;
+	}
+	
+	public TreeItem<NamedTag> loadTag(NamedTag tag) {
+		TreeItem<NamedTag> item = new TreeItem<NamedTag>(tag);
+		if (tag.getTag().getID() == 9) {
+			for (Tag<?> subTag : (ListTag<?>) tag.getTag()) {
+				item.getChildren().add(loadTag(new NamedTag("", subTag)));
+			}
+		} else if (tag.getTag().getID() == 10) {
+			CompoundTag compound = (CompoundTag) tag.getTag();
+			for (String name : compound.keySet()) {
+				item.getChildren().add(loadTag(new NamedTag(name, compound.get(name))));
+			}
+		}
+		return item;
+	}
+	
+	
+	private Image getTagImage(byte tagID) {
+		if (tagID == 1) {
+			return BYTE_ICON;
+		} else if (tagID == 2) {
+			return SHORT_ICON;
+		} else if (tagID == 3) {
+			return INT_ICON;
+		} else if (tagID == 4) {
+			return LONG_ICON;
+		} else if (tagID == 5) {
+			return FLOAT_ICON;
+		} else if (tagID == 6) {
+			return DOUBLE_ICON;
+		} else if (tagID == 7) {
+			return BYTE_ARRAY_ICON;
+		} else if (tagID == 8) {
+			return STRING_ICON;
+		} else if (tagID == 9) {
+			return LIST_ICON;
+		} else if (tagID == 10) {
+			return COMPOUND_ICON;
+		} else if (tagID == 11) {
+			return INT_ARRAY_ICON;
+		} else if (tagID == 12) {
+			return LONG_ARRAY_ICON;
 		} else {
-			throw new AssertionError("Unknown tag ID: "+tag.getID());
+			return null;
 		}
-	}
-	
-	protected TreeItem<String> loadByteTag(ByteTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(BYTE_ICON));
-	}
-	
-	protected TreeItem<String> loadShortTag(ShortTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(SHORT_ICON));
-	}
-	
-	protected TreeItem<String> loadIntTag(IntTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(INT_ICON));
-	}
-	
-	protected TreeItem<String> loadLongTag(LongTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(LONG_ICON));
-	}
-	
-	protected TreeItem<String> loadFloatTag(FloatTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(FLOAT_ICON));
-	}
-	
-	protected TreeItem<String> loadDoubleTag(DoubleTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(DOUBLE_ICON));
-	}
-	
-	protected TreeItem<String> loadByteArrayTag(ByteArrayTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(BYTE_ARRAY_ICON));
-	}
-	
-	protected TreeItem<String> loadStringTag(StringTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(STRING_ICON));
-	}
-	
-	protected TreeItem<String> loadListTag(ListTag<?> tag, String name) {
-		TreeItem<String> item = new TreeItem<>(name+": "+tag.size()+" entries", new ImageView(LIST_ICON));
-		for (Tag<?> subTag : tag) {
-			item.getChildren().add(loadTag(new NamedTag("", subTag)));
-		}
-		return item;
-	}
-	
-	protected TreeItem<String> loadCompoundTag(CompoundTag tag, String name) {
-		TreeItem<String> item = new TreeItem<>(name+": "+tag.size()+" entries", new ImageView(COMPOUND_ICON));
-		for (String s : tag.keySet()) {
-			item.getChildren().add(loadTag(new NamedTag(s, tag.get(s))));
-		}
-		return item;
-	}
-	
-	protected TreeItem<String> loadIntArrayTag(IntArrayTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(INT_ARRAY_ICON));
-	}
-	
-	protected TreeItem<String> loadLongArrayTag(LongArrayTag tag, String name) {
-		return new TreeItem<>(name+": "+tag.valueToString(), new ImageView(LONG_ARRAY_ICON));
-	}
+ 	}
 	
 	public Parent getRoot() {
 		return root;
