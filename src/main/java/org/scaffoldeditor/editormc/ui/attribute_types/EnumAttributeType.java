b99ae7a08bfd72c4c953693d559c1faa8542e576
Igrium <thesam54123@gmail.com>
2021-06-06 12:36:05 -0700

    added enum attribute renderer

---
+++ src/main/java/org/scaffoldeditor/editormc/ui/attribute_types/EnumAttributeType.java
@@ -0,0 +1,42 @@
+package org.scaffoldeditor.editormc.ui.attribute_types;
+
+import org.scaffoldeditor.editormc.ui.EntityEditor;
+import org.scaffoldeditor.scaffold.level.entity.attribute.Attribute;
+import org.scaffoldeditor.scaffold.level.entity.attribute.EnumAttribute;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+
+public class EnumAttributeType implements IRenderAttributeType {
+	
+	// We're not doing anything with the enum before sending it to code that's blind
+	// to what class it is, so typing or checking it won't do anything.
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public Node createSetter(String name, Attribute<?> defaultValue) {
+		if (!(defaultValue instanceof EnumAttribute)) {
+			return EntityEditor.DEFAULT_ATTRIBUTE_TYPE.createSetter(name, defaultValue);
+		}
+		
+		EnumAttribute<?> attribute = (EnumAttribute<?>) defaultValue;
+		ChoiceBox<String> choiceBox = new ChoiceBox<>();
+		
+		for (Enum<?> value : attribute.getEnumClass().getEnumConstants()) {
+			choiceBox.getItems().add(value.toString());
+		}
+		choiceBox.setValue(attribute.getValue().toString());
+		
+		choiceBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				Enum<?> newEnum = Enum.valueOf(attribute.getEnumClass(), newValue);
+				choiceBox.fireEvent(new ChangeAttributeEvent(ChangeAttributeEvent.ATTRIBUTE_CHANGED, name, new EnumAttribute(newEnum)));
+			}
+		});
+		
+		return choiceBox;
+	}
+
+}
